deec32fda381514e606675ebd00905a1e9c0802c
gfortin <gfortin@keyneosoft.fr>
2020-04-23 19:27:26 +0200

    Register to codex config server

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
